f5a5bd1f8fb8a9d0e9d70b94d2d89e22972cf0e0
CVilares <>
2024-01-23 17:24:24 +0000

    added product views and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def all_products(request):
+    """a view to show all product, including sorting and queries"""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
