9c9f2bfff48c6f8169c4145296bbe0f04155059a
Carlos vilares <car.vilares@gmail.com>
2024-02-02 12:53:15 +0000

    admin product editing products

--- products/urls.py
+++ products/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
     path('order_history/', order_history, name='order_history'),
-    
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     
 ]
