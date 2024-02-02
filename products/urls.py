77e1645c90d2d8e005722b4a446aecccf21ec978
Carlos vilares <car.vilares@gmail.com>
2024-02-02 13:09:30 +0000

    product admin delete product

--- products/urls.py
+++ products/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('order_history/', order_history, name='order_history'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
-    
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
