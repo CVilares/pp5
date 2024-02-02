ddbbf4d6336e917bfd5cad77f81fd047ffc46393
Carlos vilares <car.vilares@gmail.com>
2024-02-02 23:33:08 +0000

    removed secret key and set debug

--- your_skin/settings.py
+++ your_skin/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-re2(1ez&%_g$j6^0zywf7h$l(bki784lpk2p7e^24#i*$b@40('
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
